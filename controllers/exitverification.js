b3385c88a1fc82ea43f44c4db66590a44cda315f
AravindRajalingam <146104883+AravindRajalingam@users.noreply.github.com>
2025-10-05 18:52:31 +0530

    skip lunch break

--- controllers/exitverification.js
+++ controllers/exitverification.js
@@ -116,13 +116,13 @@ const Exitverification = async (req, res) => {
     const [h, m] = exitTime.split(":").map(Number);
     const currentMinutes = h * 60 + m;
     // Define the restricted range (12:50 to 14:00)
-  //   const skipStartMinutes = 12 * 60 + 50; // 12:50 → 770
-  //   const skipEndMinutes = 14 * 60 + 0;    // 14:00 → 840
+    const skipStartMinutes = 12 * 60 + 50; // 12:50 → 770
+    const skipEndMinutes = 14 * 60 + 0;    // 14:00 → 840
 
-  //  if (currentMinutes >= skipStartMinutes && currentMinutes <= skipEndMinutes) {
-  //     console.log("⏸ Current time within 12:50–14:00 → skipping any action");
-  //     return res.json({ success: true, message: "No action performed during break time" });
-  //   }
+   if (currentMinutes >= skipStartMinutes && currentMinutes <= skipEndMinutes) {
+      console.log("⏸ Current time within 12:50–14:00 → skipping any action");
+      return res.json({ success: true, message: "No action performed during break time" });
+    }
 
     console.log(`End time for dept: ${end_time_str} (${endMinutes} min)`);
     console.log(`Current time: ${exitTime} (${currentMinutes} min)`);
@@ -166,7 +166,8 @@ const CheckAfterEndGrace = async (req, res) => {
 
     const now = new Date();
     const today = now.toLocaleDateString("en-CA");
-    const exitTime = now.toLocaleTimeString("en-GB", { hour12: false });
+   const exitTime = now.toLocaleTimeString("en-GB", {hour12: false,timeZone: "Asia/Kolkata", // Chennai is in the same timezone
+});
 
     // 2. Check if student still exists in location without exit_time
     const { data: locationData, error: locError } = await supabase
